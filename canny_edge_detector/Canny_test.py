7608a572528b8139ff521798719f076e9fbea025
Alexander Vilesov <avilesov@usc.edu>
2019-11-20 19:03:32 -0800

    serial_python_version

---
+++ canny_edge_detector/Canny_test.py
@@ -0,0 +1,21 @@
+from utils import utils
+import canny_edge_detector as ced
+from timeit import default_timer as timer
+
+imgs = utils.load_data()
+#utils.visualize(imgs, 'gray')
+
+
+
+
+detector = ced.cannyEdgeDetector(imgs, sigma=10, kernel_size=5, lowthreshold=0.09, highthreshold=0.17, weak_pixel=250)
+
+
+start = timer()
+
+imgs_final = detector.detect()
+
+end = timer()
+print(end-start)
+
+utils.visualize(imgs_final, 'gray')
