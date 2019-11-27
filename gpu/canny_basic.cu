6dbe1610bf51edfc7a3d8f4ad6e825f76e85847b
Alexander Vilesov <avilesov@galilei.usc.edu>
2019-11-27 15:00:07 -0800

    gaussian kernel pipeline and non_max_suppression init

--- gpu/canny_basic.cu
+++ gpu/canny_basic.cu
@@ -1,12 +1,15 @@
 
 
 #include<string>
+#include<map>
 #include<iostream> 
+#include<math.h>
 #include<stdlib.h>
 #include <cublas.h> //CUDA
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+
 
 using namespace cv;
 using namespace std;
@@ -34,7 +37,7 @@ string type2str(int type) {
   return r;
 }
 
-__global__ void convolution_kernel(int *img, int *conv, int *h, int *w, int *padding){
+__global__ void convolution_kernel(int *img, int *conv, int *phase, int *h, int *w, int *padding){
 	int my_x = threadIdx.x;
 	int my_y = (blockIdx.x+*padding)*(blockDim.x+2*(*padding));
 	int x_gradient[] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
@@ -63,28 +66,48 @@ __global__ void convolution_kernel(int *img, int *conv, int *h, int *w, int *pad
 	// }
 
 	conv[(my_y) + my_x + *padding] = int(sqrt(float(y_mag*y_mag)+ float(x_mag*x_mag))/758*255); //CUDA only accpets floats/double in fp operations
+	float phase_angle = atan2(float(y_mag), float(x_mag)) * 180 / (atan(1.0)*4);
+	if ( phase_angle < 0){
+		phase_angle += 180;
+	}
+	phase[(my_y) + my_x + *padding] = phase_angle;
 }
 
+//3x3 Kernel hard-coded with 1-sigma SD
 __global__ void gaussian_filter(int *img, int *conv, int *padding){
 	int my_x = threadIdx.x;
 	int my_y = (blockIdx.x+*padding)*(blockDim.x+2*(*padding));
 	
-	int gauss[] = 
+	float gauss[] = {0.077847,	0.123317,	0.077847,
+					0.123317,	0.195346,	0.123317,
+					0.077847,	0.123317,	0.077847};
 
-	gauss_val = img[(my_y-blockDim.x+2*(*padding))+my_x+*padding-1]*x_gradient[0] +  img[(my_y-blockDim.x+2*(*padding))+my_x+*padding]*x_gradient[1] + img[(my_y-blockDim.x+2*(*padding))+my_x+*padding+1]*x_gradient[2] +  
-			img[my_y+my_x+*padding-1]*x_gradient[3] +  img[my_y+my_x+*padding]*x_gradient[4] + img[my_y+my_x+*padding+1]*x_gradient[5] + 
-			img[(my_y+blockDim.x+2*(*padding))+my_x+*padding-1]*x_gradient[6] +  img[(my_y+blockDim.x+2*(*padding))+my_x+*padding]*x_gradient[7] + img[(my_y+blockDim.x+2*(*padding))+my_x+*padding+1]*x_gradient[8];
+	float gauss_val = 	img[(my_y-blockDim.x+2*(*padding))+my_x+*padding-1]*gauss[0] +  img[(my_y-blockDim.x+2*(*padding))+my_x+*padding]*gauss[1] + img[(my_y-blockDim.x+2*(*padding))+my_x+*padding+1]*gauss[2] +  
+						img[my_y+my_x+*padding-1]*gauss[3] +  img[my_y+my_x+*padding]*gauss[4] + img[my_y+my_x+*padding+1]*gauss[5] + 
+						img[(my_y+blockDim.x+2*(*padding))+my_x+*padding-1]*gauss[6] +  img[(my_y+blockDim.x+2*(*padding))+my_x+*padding]*gauss[7] + img[(my_y+blockDim.x+2*(*padding))+my_x+*padding+1]*gauss[8];
 
-	
-
-	// if (int(sqrt(float(y_mag*y_mag))) > 255){
-	// 	y_mag = 0;
-	// }
-
-	conv[(my_y) + my_x + *padding] = int(gauss_val); //CUDA only accpets floats/double in fp operations
+	conv[(my_y) + my_x + *padding] = int(gauss_val); 
 
 }
 
+__global__ void non_max_suppression(int *img, int *phase, int *output, int *padding){
+	int my_x = threadIdx.x;
+	int my_y = (blockIdx.x+*padding)*(blockDim.x+2*(*padding));
+
+	int phase_to_pix[8][4] = {{0,1,0,-1} , {-1,1,1,-1}, {-1,1,1,-1}, {1,0,-1,0}, {1,0,-1,0}, {1,1,-1,-1}, {1,1,-1,-1}, {0,1,0,-1}};
+
+	//Finish with %22.5
+
+	}
+
+
+
+
+// }
+
+struct pixel_angle{
+	int pixel_loc[4];
+};
 
 
 int main(){
@@ -99,7 +122,7 @@ int main(){
 	Mat img_cv = imread("test.png");
 	Mat gray_img;
 	cvtColor(img_cv, gray_img, CV_BGR2GRAY);
-	//printf("%s", type2str(gray_img.type()).c_str());
+	printf("%s", type2str(gray_img.type()).c_str());
 
 	int height = img_cv.rows;
 	int width = img_cv.cols;
@@ -108,13 +131,12 @@ int main(){
 
 	int *img = new int[height*width];
 	int *conv_img = new int[height*width];
+	int *phase_img = new int[height*width];
 
 	int i = 0;
 	for(i = 0; i < height*width; i++){
 		img[i] = gray_img.at<uchar>(int(i/width), i%width);
 	}
-
-	printf("%i\n", conv_img[812]);
 
 	//Kernel Setup
 	int kernel_size = 3;
@@ -127,22 +149,51 @@ int main(){
 	dim3 dimGrid(height-2*padd);
 	dim3 dimBlock(width-2*padd);
 
-	int *gpu_img, *gpu_conv_img, *gpu_padd, *gpu_h, *gpu_w;
+	int *gpu_img, *gpu_conv_img, *gpu_phase_img, *gpu_padd, *gpu_h, *gpu_w;
 	cudaMalloc((void**)&gpu_img, sizeof(int)*height*width);
 	cudaMalloc((void**)&gpu_conv_img, sizeof(int)*height*width);
+	cudaMalloc((void**)&gpu_phase_img, sizeof(int)*height*width);
 	cudaMalloc((void**)&gpu_padd, sizeof(int));
 	cudaMalloc((void**)&gpu_h, sizeof(int));
 	cudaMalloc((void**)&gpu_w, sizeof(int));
+	// map<int,int*> gpu_angle_pix;
+	// map<int,int*> angle_to_pixel_loc;
+	// int NS[] {0,1,0,-1};
+	// angle_to_pixel_loc[0] = NS;
+	// cudaMalloc((void**)&gpu_angle_pix, sizeof(angle_to_pixel_loc));
+
 
 	cudaMemcpy(gpu_img, img, sizeof(int)*height*width, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_conv_img, conv_img, sizeof(int)*height*width, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_phase_img, phase_img, sizeof(int)*height*width, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_padd, padd_p, sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_h, h_p, sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(gpu_w, w_p, sizeof(int), cudaMemcpyHostToDevice);
 
-	//invoke Kernel
-	convolution_kernel<<<dimGrid, dimBlock>>> (gpu_img, gpu_conv_img, gpu_h, gpu_w, gpu_padd);
+	//invoke Gauss Kernel
+	gaussian_filter<<<dimGrid, dimBlock>>> (gpu_img, gpu_conv_img, gpu_padd);
 	cudaMemcpy(conv_img, gpu_conv_img, sizeof(int)*height*width, cudaMemcpyDeviceToHost);
+
+	// //invoke Sobel Kernel
+	convolution_kernel<<<dimGrid, dimBlock>>> (gpu_img, gpu_conv_img, gpu_phase_img, gpu_h, gpu_w, gpu_padd);
+	cudaMemcpy(conv_img, gpu_conv_img, sizeof(int)*height*width, cudaMemcpyDeviceToHost);
+	cudaMemcpy(phase_img, gpu_phase_img, sizeof(int)*height*width, cudaMemcpyDeviceToHost);
+
+	// invoke non-max suppression
+	// map<int, pixel_angle> angle_to_pixel_loc;
+	// pixel_angle NS, EW, NESW, NWSE;
+	// NS.pixel_loc = {0,1,0,-1};
+	// EW.pixel_loc = {1,0,-1,0};
+	// NESW.pixel_loc = {1,1,-1,-1};
+	// NWSE.pixel_loc = {-1,1,1,-1};
+
+
+	printf("%i\n", phase_img[1000]);
+
+
+
+
+
 
 	//find max element
 	printf("%i\n", *max_element(conv_img, conv_img + height*width));
@@ -175,11 +226,14 @@ int main(){
 
 	delete[] img;
 	delete[] conv_img;
+	delete[] phase_img;
 	cudaFree(gpu_img);
 	cudaFree(gpu_conv_img);
 	cudaFree(gpu_padd);
 	cudaFree(gpu_h);
 	cudaFree(gpu_w);
+	cudaFree(gpu_phase_img);
+	// cudaFree(gpu_angle_pix);
 
 	return 0;
 }
